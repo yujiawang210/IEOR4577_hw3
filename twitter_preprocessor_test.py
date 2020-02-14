4eb32c5e9bf2884dbe3a08aeb354c29486390d14
Yujia Wang <yw3400@columbia.edu>
2020-02-14 17:41:18 -0500

    add twitter preprocessor test

---
+++ twitter_preprocessor_test.py
@@ -0,0 +1,49 @@
+"""
+Testing Tweets Preprocessor
+"""
+import unittest
+from twitter_preprocessor import TwitterPreprocessor
+from test_case_generator import TestCasesGenerator
+
+class Tests(unittest.TestCase):
+    """define testing class"""
+
+    def setUp(self):
+        return
+
+    def test_clean_text(self):
+        """testing cleaning"""
+        for test_case in getattr(TestCasesGenerator, 'clean'):
+            tweet = TwitterPreprocessor(test_case['case'])
+            tweet.clean_text()
+            result = tweet.text
+            expected_result = test_case['expected']
+            self.assertEqual(result, expected_result)
+
+
+    def test_tokenize_text(self):
+        """testing tokenizing"""
+        for test_case in getattr(TestCasesGenerator, 'tokenize'):
+            tweet = TwitterPreprocessor(test_case['case'])
+            tweet.tokenize_text()
+            result = tweet.text
+            expected_result = test_case['expected']
+            self.assertEqual(result, expected_result)
+
+    def test_replace_token_with_index(self):
+        """testing indexing"""
+        for test_case in getattr(TestCasesGenerator, 'index'):
+            tweet = TwitterPreprocessor(test_case['case'])
+            tweet.replace_token_with_index()
+            result = tweet.text
+            expected_result = test_case['expected']
+            self.assertEqual(result, expected_result)
+
+    def test_pad_sequence(self):
+        """testing pad sequence"""
+        for test_case in getattr(TestCasesGenerator, 'padding'):
+            tweet = TwitterPreprocessor(test_case['case'])
+            tweet.pad_sequence()
+            result = tweet.text
+            expected_result = test_case['expected']
+            self.assertEqual(result, expected_result)
