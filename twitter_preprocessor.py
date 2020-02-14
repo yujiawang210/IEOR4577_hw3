ab20ec88c315e6d9546ce5080458eea63c1d37c0
Yujia Wang <yw3400@columbia.edu>
2020-02-14 17:40:31 -0500

    update tweet preprocessor

--- twitter_preprocessor.py
+++ twitter_preprocessor.py
@@ -1,72 +1,80 @@
-# ------ Import Required Libraries ------
+"""
+Tweets Preprocessor
+"""
 import os
-import string
-import numpy as np
 from nltk import re
-from nltk.corpus import stopwords
 from nltk.tokenize import TweetTokenizer
 
-
-# ------ GloVe embedding dictionary ------
-glove_dim = 100  
-root_path = os.getcwd()
-glove_file = '/glove.twitter.27B/glove.twitter.27B.' + str(glove_dim) + 'd.txt'
-
-emb_index = {}
-glove = open(root_path+glove_file)
-for row, line in enumerate(glove):
+GLOVE_DIM = 100
+ROOT_PATH = os.getcwd()
+GLOVE_FILE = '/glove.twitter.27B/glove.twitter.27B.' + str(GLOVE_DIM) + 'd.txt'
+EMB_INDEX = {}
+GLOVE = open(ROOT_PATH+GLOVE_FILE)
+for row, line in enumerate(GLOVE):
     values = line.split()
     word = values[0]
-    emb_index[word] = row
-glove.close()
+    EMB_INDEX[word] = row
+GLOVE.close()
 
-
-# ------ Define TwitterPreprocessor Class ------
 class TwitterPreprocessor:
-    
+    """define twitter preprocessor Class"""
+
     # - dunder function -
-    def __init__(self, text:str, max_length_tweet=30, max_length_dictionary=1000):
+    def __init__(self, text: str, max_length_tweet=30, max_length_dictionary=1000):
         self.text = text
         self.max_length_tweet = max_length_tweet
         self.max_length_dictionary = max_length_dictionary
-    
+
     # - supporting functions -
     def remove_url(self):
+        """remove urls"""
         pattern = re.compile(r'https?:\/\/[A-Za-z0-9\/\.\-\#]*')
         self.text = re.sub(pattern=pattern, repl='', string=self.text)
         return self
-        
+
     def remove_hashtag(self):
+        """remove hashtags"""
         pattern = re.compile(r'#\w*')
         self.text = re.sub(pattern=pattern, repl='', string=self.text)
         return self
-    
+
     def remove_mentions(self):
+        """remove @ mentions"""
         pattern = re.compile(r'@\w*')
         self.text = re.sub(pattern=pattern, repl='', string=self.text)
         return self
-    
+
     def remove_twitter_handle(self):
+        """remove twitter handles"""
         pattern = re.compile(r'RT')
         self.text = re.sub(pattern=pattern, repl='', string=self.text)
         return self
-    
+
     def remove_numbers(self):
+        """remove any numbers"""
         pattern = re.compile(r'[0-9]+')
         self.text = re.sub(pattern=pattern, repl='', string=self.text)
         return self
-        
+
     def remove_punctuation(self):
+        """remove any punctutations"""
         pattern = re.compile(r'[^\w\s]')
         self.text = re.sub(pattern=pattern, repl='', string=self.text)
         return self
-        
+
     def lower(self):
+        """lower text"""
         self.text = self.text.lower()
         return self
-    
+
+    def strip(self):
+        """strip text"""
+        self.text = self.text.strip()
+        return self
+
     # - twitter preprocessor main methods -
     def clean_text(self):
+        """cleaning the raw text"""
         return self\
             .remove_url()\
             .remove_hashtag()\
@@ -74,19 +82,23 @@ class TwitterPreprocessor:
             .remove_twitter_handle()\
             .remove_numbers()\
             .remove_punctuation()\
-            .lower()
+            .lower()\
+            .strip()
 
     def tokenize_text(self):
+        """converting a string into an array of tokens"""
         tknzr = TweetTokenizer()
         self.text = tknzr.tokenize(self.text)
         return self
-    
+
     def replace_token_with_index(self):
+        """replacing tokens with index"""
         for i in range(len(self.text)):
-            self.text[i] = emb_index[self.text[i]]
+            self.text[i] = EMB_INDEX[self.text[i]]
         return self
-    
+
     def pad_sequence(self):
+        """padding a list of indices with 0 until a maximum length"""
         max_length_tweet = self.max_length_tweet
         if len(self.text) < max_length_tweet:
             self.text.extend([0] * (max_length_tweet - len(self.text)))
